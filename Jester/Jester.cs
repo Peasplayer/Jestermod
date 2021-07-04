18180bbf1245c1e0422c674da02d639b17c85346
Peasplayer <63916422+Peasplayer@users.noreply.github.com>
2021-07-04 21:33:04 +0200

    Uploaded everything

---
+++ Jester/Jester.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx.IL2CPP;
+using HarmonyLib;
+using PeasAPI;
+using PeasAPI.Components;
+using PeasAPI.CustomEndReason;
+using PeasAPI.Roles;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jester
+{
+    [RegisterCustomRole]
+    class Jester : BaseRole
+    {
+        public Jester(BasePlugin plugin) : base(plugin) { }
+
+        public override string Name => "Jester";
+
+        public override string Description => "Trick the crew";
+
+        public override Color Color => new Color(136f / 256f, 31f / 255f, 136f / 255f);
+
+        public override int Limit => (int) JesterPlugin.JesterAmount.GetValue(); //This is a CustomNumberOption from Reactor-Essentials. You need to make it in the Main class so it gets loaded.
+                                                                                  //The code for that option is this: public static CustomNumberOption JesterAmount = CustomOption.AddNumber("JesterAmount", "Jesters", true, 1, 1, 3, 1);
+
+        public override Team Team => Team.Alone;
+
+        public override Visibility Visibility => Visibility.NoOne;
+
+        public override void OnGameStart()
+        {
+            //Gets called when the game starts.
+        }
+
+        public override void OnUpdate()
+        {
+            //Gets called every frame.
+        }
+
+        public override void OnMeetingUpdate(MeetingHud meeting)
+        {
+            //Gets called every frame when a meeting is active. The meeting gets passed on.
+        }
+
+        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.Exiled))]
+        class PlayerControlExiledPatch
+        {
+            public static void Prefix(PlayerControl __instance)
+            {
+                if (__instance.IsRole<Jester>())
+                {
+                    var winners = new List<byte>();
+                    foreach (var player in PlayerControl.AllPlayerControls)
+                    {
+                        if (player.IsRole<Jester>())
+                            winners.Add(player.PlayerId);
+                    }
+
+                    new CustomEndReason(new Color(136f / 256f, 31f / 255f, 136f / 255f), winners, "impostor");
+                }
+            }
+        }
+    }
+}
---
+++ Jester/JesterPlugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using Essentials.Options;
+using HarmonyLib;
+using PeasAPI;
+using PeasAPI.Components;
+using Reactor;
+
+namespace Jester
+{
+    [BepInPlugin(Id)]
+    [BepInProcess("Among Us.exe")]
+    [BepInDependency(ReactorPlugin.Id)]
+    public class JesterPlugin : BasePlugin
+    {
+        public const string Id = "me.change.please";
+
+        public Harmony Harmony { get; } = new Harmony(Id);
+
+        public static CustomNumberOption JesterAmount = CustomOption.AddNumber("JesterAmount", "Jesters", true, 1, 1, 3, 1);
+
+        public override void Load()
+        {
+            Watermark.PingText = "Jestermod by Peasplayer";
+            Watermark.VersionText = "Jestermod by Peasplayer";
+
+            CustomServerManager.RegisterServer("Peaspowered", "au.peasplayer.tk", 22023);
+
+            RegisterCustomRoleAttribute.Register(this);
+
+            Harmony.PatchAll();
+        }
+    }
+}
